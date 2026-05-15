a3f04b10babfb7a05d1728ba4149c3fcb1f9068a
JCPrime01 <vanzokiller@gmail.com>
2026-05-14 23:07:31 -0300

    update shareCode to F4Z44T3S14U

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=DGJ49AXLVOT&afp2=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=F4Z44T3S14U&afp2=tapi2' });
     res.end();
   }
