90af4d51c5c45dfb96913c3fcc81e3d02b411d1e
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-23 20:55:19 -0300

    chore: update shareCode to X82GSQ0CHS3

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=KXZLO0DSTZ7&afp2=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=X82GSQ0CHS3&afp2=tapi2' });
     res.end();
   }
