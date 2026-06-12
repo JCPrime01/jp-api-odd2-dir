34bdaf2ec34f762c6d739c78bba2a7006b04e470
JCPrime01 <vanzokiller@gmail.com>
2026-06-11 22:14:42 -0300

    chore: shareCode PBQCEIRRXT7 -> V9BU45ABUDF (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=PBQCEIRRXT7&afp2=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=V9BU45ABUDF&afp2=ta' });
     res.end();
   }
