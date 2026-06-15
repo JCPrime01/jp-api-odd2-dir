5a0b8af3ed5318ed258d442ceb5c8f1ca6bdb4d6
JCPrime01 <vanzokiller@gmail.com>
2026-06-14 22:31:53 -0300

    chore: shareCode 9UM4OO907W4 -> HS4MNEMIYGQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=9UM4OO907W4&afp2=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=HS4MNEMIYGQ&afp2=ta' });
     res.end();
   }
