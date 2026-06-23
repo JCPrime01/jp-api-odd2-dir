f7bda86c6dc1fdb3a5ea9c1ef63de0f07dd5fb72
JCPrime01 <vanzokiller@gmail.com>
2026-06-23 01:01:36 -0300

    chore: shareCode ON51UVRLQVP -> JV9UMH3JXNM (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=ON51UVRLQVP&afp2=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=JV9UMH3JXNM&afp2=ta' });
     res.end();
   }
