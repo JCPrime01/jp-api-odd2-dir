eb027d4956cc74a13249e5fe5c9566acfc97e50d
JCPrime01 <vanzokiller@gmail.com>
2026-06-27 23:49:32 -0300

    chore: shareCode QDO5PWRFWHL -> L45G0SUSCL8 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=QDO5PWRFWHL&afp2=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=L45G0SUSCL8&afp2=ta' });
     res.end();
   }
