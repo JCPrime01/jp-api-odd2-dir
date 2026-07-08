79db8aa2f7a33adf229a8ef22fb15ea2c1f32a42
JCPrime01 <vanzokiller@gmail.com>
2026-07-07 23:31:17 -0300

    chore: shareCode EJEZAPLOKQQ -> KRTCKCZ3HXR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=EJEZAPLOKQQ&afp2=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=KRTCKCZ3HXR&afp2=ta' });
     res.end();
   }
