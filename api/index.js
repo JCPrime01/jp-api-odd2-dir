364dbf932d2c0c0f9de857119389db7b002b458b
JCPrime01 <vanzokiller@gmail.com>
2026-07-11 23:11:13 -0300

    chore: shareCode R8PYS5U764W -> 8BSUBR4EZVS (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=R8PYS5U764W&afp2=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=8BSUBR4EZVS&afp2=ta' });
     res.end();
   }
