ee6453e463a76a771de16738f008e090b8c18e2d
JCPrime01 <vanzokiller@gmail.com>
2026-07-13 22:36:58 -0300

    chore: shareCode X78L2958O2F -> P2IEKUVI27I (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=X78L2958O2F&afp2=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=P2IEKUVI27I&afp2=ta' });
     res.end();
   }
