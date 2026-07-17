a4737e87b846e40779fccb14ed37e345cdc56f7b
JCPrime01 <vanzokiller@gmail.com>
2026-07-17 00:23:45 -0300

    chore: shareCode JPXQYXM0Q47 -> ALI0VV4G5VR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=JPXQYXM0Q47&afp2=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=ALI0VV4G5VR&afp2=ta' });
     res.end();
   }
