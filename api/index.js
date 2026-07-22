d49203ed94a486195b86f661b166c5374e3c1b00
JCPrime01 <vanzokiller@gmail.com>
2026-07-22 05:05:04 -0300

    chore: shareCode HVZKJURTNET -> 6L9RUAEEPJB (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=HVZKJURTNET&afp2=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=6L9RUAEEPJB&afp2=ta' });
     res.end();
   }
